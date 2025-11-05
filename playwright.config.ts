77da03e18924e99432f3f8f7736de3fbfcdc49b6
Carl Eslabon <carleslabon@gmail.com>
2025-11-06 06:05:31 +0800

    Modified Playwright Configuration File

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,14 @@ export default defineConfig({
   testDir: './tests',
   timeout: 60000,
   retries: 1,
+  fullyParallel: true,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     browserName: 'chromium',
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    trace: 'on-first-retry'
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
